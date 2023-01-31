b1ad87efc8baa1d27d8554c202280b6c1003134b
Goodnessmbakara <amicablembakara50@gmail.com>
2023-01-31 21:40:17 +0100

    a hot fix

--- contactForm/contact_api/serializers.py
+++ contactForm/contact_api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 class EmailSerializer(serializers.Serializer):
+    full_name = serializers.CharField(max_length=255)
     subject = serializers.CharField(max_length=100)
     message = serializers.CharField()
     recipient_email = serializers.EmailField(default='activation.django@gmail.com')
