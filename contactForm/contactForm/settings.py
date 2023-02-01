010dc858ee1b8fe0398c4910ae83d7a6ea544574
Goodnessmbakara <amicablembakara50@gmail.com>
2023-02-01 14:19:48 +0100

    m

--- contactForm/contactForm/settings.py
+++ contactForm/contactForm/settings.py
@@ -24,7 +24,7 @@ load_dotenv(BASE_DIR / '.env')
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-lx)%l2%xyb2t4-tm7u4hl(sfdb(vgg!=hqbv+#n^a69ow5y&mf'
+
 SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
