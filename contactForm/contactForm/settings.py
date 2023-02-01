d2be7b27c92ebe78360a8898ae33f188093baff6
Goodnessmbakara <amicablembakara50@gmail.com>
2023-02-01 16:32:30 +0100

    what

--- contactForm/contactForm/settings.py
+++ contactForm/contactForm/settings.py
@@ -30,8 +30,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # DEBUG = True
 DEBUG = os.getenv('DEBUG', '0').lower() in ['true', 't', '1']
 
-ALLOWED_HOSTS = ['https://contactform-6vda.onrender.com/']
-#ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
+#ALLOWED_HOSTS = ['https://contactform-6vda.onrender.com/']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
 
 
 # Application definition
