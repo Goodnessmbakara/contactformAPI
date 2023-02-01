f766f9357cbed1c0b44af49d5a9b032063701854
Goodnessmbakara <amicablembakara50@gmail.com>
2023-02-01 12:44:28 +0100

    finally working

--- contactForm/contactForm/settings.py
+++ contactForm/contactForm/settings.py
@@ -125,8 +125,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'activation.django@example.com'
+EMAIL_HOST_USER = 'activation.django@gmail.com'
 EMAIL_HOST_PASSWORD = 'mijgmeaumygfelzz'
--- contactForm/contact_api/views.py
+++ contactForm/contact_api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework.response import Response
+from django.http import JsonResponse
 from rest_framework.views import APIView
 from django.core.mail import send_mail
 from . serializers import EmailSerializer
